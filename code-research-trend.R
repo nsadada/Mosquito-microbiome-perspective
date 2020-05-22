f90abb5f35a9c8872fd37627850aac8d8b45e274
Nsa Dada <63561583+nsadada@users.noreply.github.com>
2020-05-22 13:05:12 +0200

    Update code-research-trend.R

--- code-research-trend.R
+++ code-research-trend.R
@@ -2,7 +2,6 @@
 ## Adapted from Daniel S. Quintana: https://gist.github.com/dsquintana/b512b715786088339b61a7fb79367d5e
 
 ## Install required packages
-
 install.packages("tidyverse")
 install.packages("europepmc")
 install.packages("cowplot")
@@ -13,11 +12,9 @@ library(europepmc)
 library(cowplot)
 
 ## Scrape data from Europe PMC archive
-
 Mosquito_microbiome_research_trend <- europepmc::epmc_hits_trend(query = "mosquito microbiome", period = 2000:2020)
 
 ## Plot research trend data
-
  Mosquito_microbiome_research_trend %>%
       ggplot(aes(x = factor(year), y = (query_hits))) +
       geom_col(fill = "#660066", width = 0.7, alpha = 0.9) +
